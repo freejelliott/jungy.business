7c5ba40a3443c027f4284dde09c06a9d7c5604cd
Joshua Elliott <joshualjelliott@gmail.com>
2023-09-20 16:56:46 +1000

    business on click

--- public/sketch.js
+++ public/sketch.js
@@ -1,37 +1,65 @@
-let business;
+let businessImg;
+let businesses;
 let x;
 let y;
 let xdir;
 let ydir;
-let xstep;
-let ystep;
+let maxBusiness = 500;
+
+class BouncingImage {
+  constructor(image, x, y, xdir, ydir) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.xdir = xdir;
+    this.ydir = ydir;
+  }
+
+  draw() {
+    image(this.image, this.x, this.y);
+  }
+
+  step() {
+    this.x += 1*this.xdir;
+    this.y += 1*this.ydir;
+    if (this.x > (windowWidth-this.image.width) || this.x < 0) {
+      this.xdir *= -1;
+    }
+    if (this.y > (windowHeight-this.image.height) || this.y < 0) {
+      this.ydir *= -1;
+    }
+  }
+}
 
 function preload() {
-  business = loadImage('business.png');
+  businessImg = loadImage('business.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  businesses = [];
   x = windowWidth / 2;
   y = windowHeight / 2;
   xdir = random(-1, 1);
   ydir = random(-1, 1);
-  xstep = random(-5, 5);
-  ystep = random(-5, 5);
-  image(business, x, y);
+  businesses.push(new BouncingImage(businessImg, x, y, xdir, ydir));
 }
 
 function draw() {
   background(color('#ECEFF1'));
-  image(business, x, y);
-  x += 1*xdir;
-  y += 1*ydir;
-  if (x > (windowWidth-business.width) || x < 0) {
-    xdir *= -1;
+  for (let i = 0; i < businesses.length; i++) {
+    businesses[i].step();
+    businesses[i].draw();
   }
-  if (y > (windowHeight-business.height) || y < 0) {
-    ydir *= -1;
+}
+
+function mouseClicked() {
+  if (businesses.length >= maxBusiness) {
+    return;
   }
+  xdir = random(-1, 1);
+  ydir = random(-1, 1);
+  businesses.push(new BouncingImage(businessImg, mouseX, mouseY, xdir, ydir));
 }
 
 function windowResized() {
